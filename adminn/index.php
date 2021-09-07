0034496819162308dd5a8cf58d08a2609ac4c6bb
Muhammad Alvi Awliya <muhammadalvi@users.noreply.github.com>
2021-09-07 10:38:31 +0700

    perbaikan home

--- adminn/index.php
+++ adminn/index.php
@@ -48,7 +48,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="index.html" class="brand-link">
+      <a href="index.php" class="brand-link">
         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Desa Kemutug Lor</span>
       </a>
@@ -90,7 +90,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a href="#" class="nav-link">
+              <a href="wisata.php" class="nav-link">
                 <i class="nav-icon fas fa-map-marked-alt"></i>
                 <p>
                   Wisata
